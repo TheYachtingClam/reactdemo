7bddfca4e2534c8ba5bf5e66e3f0407276e1f27c
Nicholas Mayne <nick@modelrocket.io>
2021-11-17 16:37:24 -0800

    Button and Header finished

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,24 @@
-import PropTypes from 'prop-types'
+import PropTypes from "prop-types";
+import Button from "./Button";
 
 const Header = ({ title }) => {
-    return (
-        <header>
-            <h1>{title}</h1>
-        </header>
-    )
-}
+  const onClick = () => {
+    console.log("bob");
+  };
+  return (
+    <header className="header">
+      <h1>{title}</h1>
+      <Button color="green" text="Add" onClick={onClick} />
+    </header>
+  );
+};
 
 Header.defaultProps = {
-    title: 'Task Tracker',
-}
+  title: "Task Tracker",
+};
 
 Header.propTypes = {
-    title: PropTypes.string.isRequired,
-}
+  title: PropTypes.string.isRequired,
+};
 
-export default Header
+export default Header;
